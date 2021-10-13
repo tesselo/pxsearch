bba11f7b8f499bc7b4560dc1ef0948999b7e302e
Daniel Moreno <daniel.moreno@tesselo.com>
2021-10-13 13:07:53 +0200

    refactor(db): use sensible defaults for db connection

---
+++ pxsearch/utils.py
@@ -0,0 +1,14 @@
+import os
+
+
+def get_connection_url() -> str:
+    """
+    Get connection URL from environment variables
+    (see environment variables set in docker-compose)
+    """
+    postgres_user = os.environ.get("POSTGRES_USER", "postgres")
+    postgres_pass = os.environ.get("POSTGRES_PASS", "")
+    postgres_host = os.environ.get("POSTGRES_HOST", "localhost")
+    postgres_port = os.environ.get("POSTGRES_PORT", "5432")
+    postgres_dbname = os.environ.get("POSTGRES_DBNAME", "pxsearch")
+    return f"postgresql://{postgres_user}:{postgres_pass}@{postgres_host}:{postgres_port}/{postgres_dbname}"
