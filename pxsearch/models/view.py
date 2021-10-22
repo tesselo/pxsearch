757ebc00126a34bf5c904db2eb1946c50799a9f4
Keren Vasconcelos <kerenvasconcelos@gmail.com>
2021-10-22 17:23:50 +0100

    feat(view):Removed tem_ingest_data.

--- pxsearch/models/view.py
+++ pxsearch/models/view.py
@@ -1,24 +1,26 @@
-from pxsearch.utils import get_connection_url
 from sqlalchemy import create_engine
+
+from pxsearch.utils import get_connection_url
 
 
 def create_imagery_view():
-        
+
     engine = create_engine(get_connection_url())
 
-
-    #Query definition
-    definition = """CREATE OR REPLACE VIEW data.imagery AS 
-    SELECT collection_id, 
+    # Query definition
+    definition = """CREATE OR REPLACE VIEW data.test AS
+    SELECT collection_id,
     geometry AS "bbox",
-    datetime::timestamp AS "sensing_time", 
-    properties->>'platform' AS "platform", 
-    properties->>'instruments' AS "sensor_id", 
-    properties->>'proj:transform' AS "proj_transform", 
-    properties->>'landsat:wrs_row' AS "wrs_row", 
-    properties->>'landsat:wrs_path' AS "wrs_path", 
+    datetime::timestamp AS "sensing_time",
+    properties->>'platform' AS "platform",
+    properties->>'instruments' AS "sensor_id",
+    properties->>'proj:transform' AS "proj_transform",
+    properties->>'landsat:wrs_row' AS "wrs_row",
+    properties->>'landsat:wrs_path' AS "wrs_path",
     properties->>'landsat:cloud_cover_land' AS "cloud_cover",
-    y.x->'href' AS "base_url" FROM data.items jt, LATERAL (SELECT jsonb_array_elements(links) x) y WHERE y.x @> '{"rel": "self"}';
+    y.x->'href' AS "base_url" FROM data.items jt,
+    LATERAL (SELECT jsonb_array_elements(links) x) y
+    WHERE y.x @> '{"rel": "self"}';
     """
 
     engine.execute(definition)
