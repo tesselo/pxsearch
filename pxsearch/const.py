a8ea3efb0c3325a722d9bc5ed553d3f36a887592
Daniel Wiesmann <daniel@tesselo.com>
2021-10-26 14:00:09 +0100

    feat(ls8): add Landsat-8 Level-2 ingestion script
    Also add a task to print the payload from the new scenes signal.
    This will allow to design the automatic ingestion based on SNS
    signals.

---
+++ pxsearch/const.py
@@ -0,0 +1,15 @@
+CLIENT_TYPE = "s3"
+PAGINATOR_LOOKUP = "list_objects"
+INGESTION_BATCH_SIZE = 50
+DATETIME_RFC339 = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+# LS8 L2 constants.
+LS8_L2_BUCKET_NAME = "usgs-landsat"
+LS8_L2_PAGINATOR_BASE_PREFIX = "collection02/level-2/standard/oli-tirs/"
+LS8_L2_STAC_ITEM_FILE = "_T2_SR_stac.json"
+LS8_L2_STAC_ITEM_FILE_JMES_SEARCH = (
+    f"Contents[?contains(Key, '{LS8_L2_STAC_ITEM_FILE}') == `true`].Key"
+)
+LS8_L2_COLLECTION_ID = "landsat-c2l2-sr"
+WRS_PATH_MAX = 251
+WRS_ROW_MAX = 248
