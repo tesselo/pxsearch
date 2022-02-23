99dd7068faa61b77f4cbcdba9c09869aa1bdb720
Daniel Wiesmann <daniel@tesselo.com>
2022-02-23 10:43:26 +0000

    fix(db): add pool_pre_ping setting to DB engine
    This will improve stability of the ingestion algorithm

--- pxsearch/db.py
+++ pxsearch/db.py
@@ -4,5 +4,5 @@ from sqlalchemy.orm import sessionmaker
 from pxsearch.utils import get_connection_url
 
 metadata = MetaData()
-engine = create_engine(get_connection_url())
+engine = create_engine(get_connection_url(), pool_pre_ping=True)
 session = sessionmaker(bind=engine)()
