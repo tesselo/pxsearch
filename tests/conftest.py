873f90acc4d94c7275761ed92711a846c217e455
Daniel Wiesmann <daniel@tesselo.com>
2022-05-25 09:40:20 +0100

    refactor(tests): improve db handling during testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
 import json
 
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 
+from pxsearch.db import session
 from pxsearch.utils import get_connection_url
 
 STAC_TEST_URL = "https://example.com/stac/v0"
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def database_engine():
     connection_url = get_connection_url()
     if "localhost" not in connection_url:
@@ -17,6 +18,15 @@ def database_engine():
     yield engine
     engine.execute("DROP SCHEMA data CASCADE")
     engine.execute("DROP EXTENSION IF EXISTS postgis")
+
+
+@pytest.fixture(autouse=True)
+def exploding_session(database_engine):
+    yield session
+    session.rollback()
+    for table in inspect(database_engine).get_table_names(schema="data"):
+        session.execute(f"TRUNCATE TABLE data.{table} CASCADE")
+    session.commit()
 
 
 def setup_stac_requests_mock(requests_mock):
