5816c952307c790aac3b1806bd340f7096f5b495
Daniel Wiesmann <daniel@tesselo.com>
2022-05-31 09:28:19 +0100

    feat(ingest): add option to skip already done range

--- pxsearch/ingest/run.py
+++ pxsearch/ingest/run.py
@@ -6,6 +6,7 @@ Landsat: https://landsatlook.usgs.gov/stac-server
 Sentinel-2: https://earth-search.aws.element84.com/v0
 """
 import click
+from dateutil import parser
 
 from pxsearch.ingest.stac_api import ingest_stac_collections, ingest_stac_year
 from pxsearch.utils import initialize_sentry_sdk
@@ -42,7 +43,14 @@ from pxsearch.utils import initialize_sentry_sdk
     is_flag=True,
     help="Only ingest collections.",
 )
-def ingest_year_range(url, start, end, collections):
+@click.option(
+    "-d",
+    "--already-done",
+    "already_done",
+    default=None,
+    help="Date up to which the ingestion already is done",
+)
+def ingest_year_range(url, start, end, collections, already_done):
     if collections:
         ingest_stac_collections(url)
     else:
@@ -50,8 +58,10 @@ def ingest_year_range(url, start, end, collections):
             raise click.UsageError("To ingest items provide start year.")
         if end is None:
             end = start
+        if already_done:
+            already_done = parser.parse(already_done).date()
         for year in range(start, end + 1):
-            ingest_stac_year(url, year)
+            ingest_stac_year(url, year, already_done_date)
 
 
 if __name__ == "__main__":
