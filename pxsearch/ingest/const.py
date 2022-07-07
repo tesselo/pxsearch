292800ce2fecc288e1aaddb25acd02356cbffc13
Daniel Wiesmann <daniel@tesselo.com>
2022-07-07 11:04:56 +0100

    feat(signals): centralize signal handling
    This is in preparation for serverless, where otherwise one function per signal
    is created.

--- pxsearch/ingest/const.py
+++ pxsearch/ingest/const.py
@@ -4,3 +4,5 @@ INGEST_CHUNK_SIZE = 500
 MAX_DECODE_ERROR_ATTEMPTS = 3
 STAC_SEARCH_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 ONE_DAY = datetime.timedelta(days=1)
+SENTINEL_2_SNS_ARN = "arn:aws:sns:us-west-2:608149789419:cirrus-v0-publish"
+USGS_SNS_ARN = "arn:aws:sns:us-west-2:673253540267:public-c2-notify"
